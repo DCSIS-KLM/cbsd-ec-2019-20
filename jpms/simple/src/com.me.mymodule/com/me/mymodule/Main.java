0a6cc4537efcbb8fb7f33832da8a750041271dd5
Keith Mannock <keith@dcs.bbk.ac.uk>
2019-11-02 14:18:38 +0000

    removed empty directories

--- jpms/simple/src/com.me.mymodule/com/me/mymodule/Main.java
+++
@@ -1,7 +0,0 @@
-package com.me.mymodule;
-
-public class Main {
-	public static void main(String... args){
-		System.out.println("Hello World form Module!");
-	}
-}
--- jpms/simple/src/com.me.mymodule/module-info.java
+++
@@ -1,1 +0,0 @@
-module com.me.mymodule { }
